7edb3e595aa56a3af6af7aa531fa9d27f8fe7745
qyerke <70740293+qelephant@users.noreply.github.com>
2023-02-20 15:00:01 +0600

    Update NumberReverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int m, n, sum = 0;
+        m = number;
+        while(m > 0){
+            n = m % 10;
+            sum = sum * 10 + n;
+            m = m / 10;
+        }
+        System.out.println(sum);
     }
 }
